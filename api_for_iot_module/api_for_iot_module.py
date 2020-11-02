3b361928a65cf2dca26f769d65c0eae89a783079
Cheslyn Williams <53040633+ChezzyBoi@users.noreply.github.com>
2020-11-02 21:10:10 +0200

    Update api_for_iot_module.py

--- api_for_iot_module/api_for_iot_module.py
+++ api_for_iot_module/api_for_iot_module.py
@@ -1,13 +1,6 @@
 """Main module."""
 
 from __future__ import division
-
-import sys
-import fake_rpi
-
-sys.modules['RPi'] = fake_rpi.RPi     
-sys.modules['picamera'] = fake_rpi.picamera
-
 import io
 import json
 import picamera
