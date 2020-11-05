517cae841c5d667e1f56b19847fc4b2836dddcc0
Cheslyn Williams <53040633+ChezzyBoi@users.noreply.github.com>
2020-11-05 22:00:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ChezzyBoi/api_for_iot_module',
-    version='0.1.0',
+    version='1.0.0',
     zip_safe=False,
 )
